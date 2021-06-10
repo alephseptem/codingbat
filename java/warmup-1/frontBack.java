15250540853df99df8153d841353dc28704ba7f0
alephseptem <54686642+alephseptem@users.noreply.github.com>
2021-06-11 00:05:18 +0530

    Create frontBack.java
    Given a string, return a new string where the first and last chars have been exchanged.
    
    
    frontBack("code") → "eodc"
    frontBack("a") → "a"
    frontBack("ab") → "ba"
    
    Completed on 02/02/21.

---
+++ java/warmup-1/frontBack.java
@@ -0,0 +1,7 @@
+public String frontBack(String str) {
+  if ( str.length() > 1)
+  {
+    return str.substring(str.length()-1) + str.substring(1,str.length()-1) + str.substring(0,1);
+  }
+  return str;
+}
