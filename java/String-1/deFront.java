9053f7cba56c58e6c9b74c03aef1691c48070294
minosh7 <54686642+minosh7@users.noreply.github.com>
2021-06-24 23:56:37 +0530

    Create deFront.java
    Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a' and keep the second char if it is 'b'. The string may be any length. Harder than it looks.
    
    deFront("Hello") → "llo"
    deFront("java") → "va"
    deFront("away") → "aay"
    
    Completed on 02/25/21

---
+++ java/String-1/deFront.java
@@ -0,0 +1,17 @@
+public String deFront(String str) {    
+
+if (str.substring(0,2).equals("ab"))
+{
+  return str;
+}
+if (str.substring(0,1).equals("a"))
+{
+  return str.substring(0,1) + str.substring(2);
+}
+if (str.substring(1,2).equals("b"))
+{
+  return str.substring(1,2) + str.substring(2);
+}
+
+return str.substring(2);
+}
